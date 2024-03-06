f21c92c837d7b246e58787b30e8572ccbe0591df
jaiko <khosro.jadidi@arbetsformedlingen.se>
2024-03-06 16:15:56 +0100

    finish chapter 1

--- chapter-1-why-http/Program.cs
+++ chapter-1-why-http/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
+// ReSharper disable UnusedAutoPropertyAccessor.Global
 
-Console.WriteLine("Hello, World!");
+var client = new HttpClient
+{
+    BaseAddress = new("https://api.boot.dev/v1/courses_rest_api/learn-http/items")
+};
+
+var message = new HttpRequestMessage(HttpMethod.Get, string.Empty);
+message.Headers.Add("X-API-Key", "1k4h8s6l5h3nd01l");
+// message.Headers.Add("Content-Type","application/json");
+
+var result = await client.SendAsync(message, new());
+var resultString = await result.Content.ReadAsStringAsync();
+
+var serializerOptions = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = false,
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+};
+var items = JsonSerializer.Deserialize<IEnumerable<Item>>(resultString, serializerOptions)
+    ?.ToList();
+
+ArgumentNullException.ThrowIfNull(items);
+
+foreach (var item in items)
+    Console.WriteLine(item.ToString());
+
+internal struct Item
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public int Quality { get; set; }
+
+    public override string ToString()
+    {
+        return $$"""
+                 id : {{Id}},
+                 name:{{Name}},
+                 quality:{{Quality}}
+                 """;
+    }
+}
