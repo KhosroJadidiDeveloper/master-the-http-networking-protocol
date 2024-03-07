fefad5c058437fb5681bf7d1e048b4445456eee5
jaiko <khosro.jadidi@arbetsformedlingen.se>
2024-03-07 15:48:00 +0100

    finish chapter 3

--- Chapter-3-URI/Program.cs
+++ Chapter-3-URI/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var uri = new Uri("http://dragonslayer:pwn3d@fantasyquest.com:8080/maps?sort=rank#id");
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine(uri.Scheme);
+Console.WriteLine(uri.UserInfo.Split(":")[0]);
+Console.WriteLine(uri.UserInfo.Split(":")[1]);
+Console.WriteLine(uri.Host);
+Console.WriteLine(uri.Port);
+Console.WriteLine(uri.AbsolutePath);
+Console.WriteLine(uri.Query);
+Console.WriteLine(uri.Fragment);
