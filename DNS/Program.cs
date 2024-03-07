d2653961ff243aeb635d385f3718587810cd2cb1
jaiko <khosro.jadidi@arbetsformedlingen.se>
2024-03-07 14:19:14 +0100

    finish dns chapter

--- DNS/Program.cs
+++ DNS/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
 
-Console.WriteLine("Hello, World!");
+var domain = "api.boot.dev";
+var uri = new Uri($"https://cloudflare-dns.com/dns-query?name={domain}&type=A");
+var requestMessage = new HttpRequestMessage(HttpMethod.Get, uri);
+requestMessage.Headers.Add("Accept", "application/dns-json");
+
+var client = new HttpClient();
+var response = await client.SendAsync(requestMessage);
+var responseString = await response.Content.ReadAsStringAsync();
+
+var ip = JsonDocument.Parse(responseString)
+    .RootElement
+    .GetProperty("Answer")[0]
+    .GetProperty("data")
+    .GetString();
+
+Console.WriteLine(responseString);
+Console.WriteLine(ip);
